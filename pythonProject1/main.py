a12499b163ab06393761c2d7637075cd0b7c19ce
Akash-298 <akash.c.c298@gmail.com>
2023-05-15 16:54:36 +0530

    new stuff

---
+++ pythonProject1/main.py
@@ -0,0 +1,10 @@
+import cv2
+image_path = 'C:\\Users\\mypc\\Downloads\\Airship-Sign-Up.png'
+
+image = cv2.imread(image_path)
+print(image.shape)
+# cv2.imshow('i1',image)
+#
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+
